9e8fd2a76bed753ca3285e1f0b996515e5f13fe3
Felix Dietrich <felix.dietrich@sperrhaken.name>
2013-12-28 20:09:01 +0100

    Add missing constructors to TechnisatException

--- Technisat/src/TechnisatException.java
+++ Technisat/src/TechnisatException.java
@@ -1,4 +1,17 @@
 
 @SuppressWarnings("serial")
 public class TechnisatException extends Exception {
+	public TechnisatException() {}
+	
+	public TechnisatException(String message) {
+		super(message);
+	}
+	
+	public TechnisatException(Throwable cause) {
+		super(cause);
+	}
+	
+	public TechnisatException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
